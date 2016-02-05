2168387ad76a8fc54097597698160d85f474baf1
Amanda Rincon <anrincon@ufl.edu>
2016-02-05 15:16:52 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+    <title>Web Interactivity and Engagement</title>
+
+</head>
+<body>
+    <h1>Web Interactivity and Engagement</h1>
+    <h2>Spring 2016</h2>
+
+    <p>This is where the blog will go.</p>
+
+
+</body>
+</head>
