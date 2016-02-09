9ce4300b5d6bcdb3db9dbe05d4f1e06ac069e984
Amanda Rincon <anrincon@ufl.edu>
2016-02-08 17:46:59 -0800

    adjust columns in page for sidebar

--- page.php
+++ page.php
@@ -2,11 +2,13 @@
 
     <div class="row">
         <div class="three columns">
-
+			<h2>Sidebar</h2>
+		</div>
+		<div class="nine columns">
             <?php if (have_posts()) :
                 while (have_posts()) : the_post(); ?>
                     <h2><?php the_title(); ?></h2>
-                    <?php the_excerpt();
+                    <?php the_content();
                 endwhile;
             endif; ?>
 
