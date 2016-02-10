986fe16bb0c8dcf936470ccd1c2189fd9dd3346e
Amanda Rincon <anrincon@ufl.edu>
2016-02-10 13:55:46 -0800

    add menu styling

--- header.php
+++ header.php
@@ -21,6 +21,7 @@
 		<div class="row">
 		    <div class="twelve columns">
 		        <?php wp_nav_menu(array(
+					'menu' => 'Main Menu',
 		            'sort_column' => 'menu_order',
 		            'container_class' => 'blank-menu-header'
 		            ));?>
